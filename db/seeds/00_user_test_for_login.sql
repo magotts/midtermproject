9959b0462e7a7e36da0a8952ea310b7f446473a0
Zak Warsame <zakwarsamee@gmail.com>
2021-09-19 02:22:06 +0000

    make seeda/users.sql similar to master

---
+++ db/seeds/00_user_test_for_login.sql
@@ -0,0 +1,38 @@
+-- Users table seeds here (Example)
+INSERT INTO
+  users (
+    first_name,
+    last_name,
+    phone_number,
+    password,
+    email,
+    profile_image_url
+  )
+VALUES
+  (
+    'Alice',
+    'Williams',
+    '6472212345',
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'alice.williams@gmail.com',
+    'https://www.kindpng.com/picc/m/21-214439_free-high-quality-person-icon-default-profile-picture.png'
+  );
+
+INSERT INTO
+  users (
+    first_name,
+    last_name,
+    phone_number,
+    password,
+    email,
+    profile_image_url
+  )
+VALUES
+  (
+    'Kira',
+    'Adams',
+    '4162212345',
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'kira.adams@gmail.com',
+    'https://www.kindpng.com/picc/m/21-214439_free-high-quality-person-icon-default-profile-picture.png'
+  );
