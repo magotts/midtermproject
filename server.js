bb114acc28c9e590b2e5936c2fef7f6257406677
magotts <techybet@gmail.com>
2021-09-18 22:19:28 -0400

    adding register post route

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
+const bcrypt     = require('bcrypt');
+const cookieSession = require("cookie-session");
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -22,7 +24,11 @@ db.connect();
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+app.use(express.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
@@ -55,10 +61,56 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.post("/register", (req, res) => {
+
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let phone_number = req.body.phone_number;
+  let email = req.body.email;
+  let password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  console.log(first_name, last_name, phone_number, email, hashedPassword);
+
+
+  if (first_name === "" || last_name === "" || phone_number === "" ||email === "" || password === "") {
+    return res.status(400).send("ERROR: First Name/Last Name/Phone Number/Email/Password field cannot be empty.");
+  }
+
+  const queryString = `
+    INSERT INTO users (first_name, last_name, phone_number, password, email) VALUES ($1, $2, $3, $4, $5)
+    RETURNING *`;
+  const values = [first_name, last_name, phone_number, password, email];
+  return db
+    .query(queryString, values)
+    .then((result) => {
+      req.session.user_id =  result.rows[0].id;
+      console.log("cookie is", req.session.user_id);
+      console.log("result row is", result.rows[0]);
+      res.redirect("/");
+    })
+    //.then(res.redirect("/"))
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+
+
+  //exports.addUser = addUser;
+
+});
+
+
 app.get("/menu", (req, res) => {
   res.render("menu");
 });
 
+
+// logout
+app.post("/logout", (req, res) => {
+  req.session = null;
+  // redirect home
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
