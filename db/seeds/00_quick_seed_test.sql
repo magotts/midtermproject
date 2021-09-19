534fba4e7b20ac6a35077280f2564c8f21009f75
Zak Warsame <zakwarsamee@gmail.com>
2021-09-19 02:27:59 +0000

    remove a few files from sql

--- db/seeds/00_quick_seed_test.sql
+++ db/seeds/00_quick_seed_test.sql
@@ -36,3 +36,14 @@ VALUES
     'kira.adams@gmail.com',
     'https://www.kindpng.com/picc/m/21-214439_free-high-quality-person-icon-default-profile-picture.png'
   );
+
+
+INSERT INTO orders (user_id, order_time, total_cost, order_status)
+VALUES
+('1','2021-08-02 15:41:20',60,'new'),
+('2','2021-08-02 15:50:20',50, 'accepted'),
+('1','2021-08-02 15:53:20',30,'accepted'),
+('2','2021-08-02 15:54:20',45,'new'),
+('1','2021-07-26 10:02:20',12,'new')
+
+
--- db/seeds/03_foods.sql
+++
@@ -1,4 +0,0 @@
--- INSERT INTO foods (title,price, stock, description,image_url)
--- VALUES
-
--- ('pe')
