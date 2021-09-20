6705508635e172b14377e1fb5428ab2e7c0524ba
magotts <techybet@gmail.com>
2021-09-20 19:43:58 -0400

    editing checkout page

--- db/seeds/order_details.sql
+++ db/seeds/order_details.sql
@@ -1,5 +1,5 @@
-INSERT INTO orders (user_id, order_time, total_cost. order_completed, order_status ord_status)
-VALUES (2, )
+INSERT INTO orders (user_id, order_time, total_cost. order_completed, order_status)
+VALUES (2, now(), 10, false, new,);
 
 INSERT INTO order_details (foods_id, orders_id, quantity)
 VALUES (1, 1, 5);
