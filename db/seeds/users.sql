5bfaf890e08925fa359391dae5cc87f6fa7f1803
magotts <techybet@gmail.com>
2021-09-20 12:25:03 -0400

    inserting users into users table

---
+++ db/seeds/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, phone_number, password, email)
+VALUES
+('Pizza Admin', 'Restaurant', 6472221111, 'secretpassword', 'hello@world.com'),
+('Raven', 'Kanken', 6471112222, 'password', 'aaa@aaa.com');
