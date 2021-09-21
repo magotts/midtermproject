9ab920b2347f21a8f680d87ea2753ea4a024220c
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-09-21 15:52:44 -0400

    add button for accepting new orders

--- public/scripts/admins.js
+++ public/scripts/admins.js
@@ -16,13 +16,18 @@ $(() => {
   };
 
   const createOrderElement = (order) => {
-    const $orderElement = $(`<tr>
+
+      const tableContent =`<tr>
             <td> ${order.id} </td>
             <td> ${order.order_time} </td>
             <td> $${order.total_cost} </td>
             <td> ${order.order_status} </td>
-          </tr>
-    `);
+            <td> ${order.order_status === "new" ? "<button class='accept-order'>ACCEPT</button>" : ""} </td>
+
+            </tr>`
+
+            const $orderElement = $(tableContent) ;
+
 
     return $orderElement;
 
@@ -35,15 +40,17 @@ $(() => {
       for (const elements of orders) {
         const $order = createOrderElement(elements);
         $orderTable.append($order);
+
       };
-      //must have something in the ejs that this function can reference and manipulate
-      //make a variable that references the item in the ejs file
-      // empty the container
-      //loop through the data
-      //append each item to the div/article
-      //calls createOrderElement
 
     };
+
+
+  $('.accept-order').on("click", (event) => {
+    console.log('hello accept button');
+    event.preventDefault();
+    $(".accept-order").hide();
+  });
 
     loadOrders();
 
