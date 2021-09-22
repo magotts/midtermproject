a524d50df97b4a4e534ce96030642317a9b9dccb
magotts <techybet@gmail.com>
2021-09-22 14:28:12 -0400

    adding finduser and log in if user is logged in

--- routes/placeorder-router.js
+++ routes/placeorder-router.js
@@ -3,6 +3,36 @@ const router = express.Router();
 
 const placeorderRouter = (db) => {
 
+  const findUserById = function (userId) {
+    const queryString = `
+    SELECT * FROM users WHERE id = $1
+    `;
+
+    return db
+      .query(queryString, [userId])
+      .then((res) => {
+        return res.rows.length > 0 ? res.rows[0] : null;
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  };
+
+  router.get("/", (req, res) => {
+    const user_id = req.session.user_id || "";
+
+    findUserById(user_id)
+      .then((user) => {
+        let session = req.session;
+
+        const templateVars = { session, user};
+        res.render("ordersuccess", templateVars);
+
+      });
+
+
+  });
+
   // router.post("/", (req, res) => {
   //   let cart = req.session.cart;
   //   for (let list of Object.values(cart.items)) {
