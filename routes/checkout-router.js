cadb5ce956d95ee6aa8b03e4f1056226bb400283
magotts <techybet@gmail.com>
2021-09-23 14:32:21 -0400

    fixing edit and delete function

--- routes/checkout-router.js
+++ routes/checkout-router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 // Secure authentication
 
 const checkoutRouter = (db) => {
-
   const findUserById = function (userId) {
     const queryString = `
     SELECT * FROM users WHERE id = $1
@@ -23,55 +22,77 @@ const checkoutRouter = (db) => {
   router.get("/", (req, res) => {
     const user_id = req.session.user_id || "";
 
-    findUserById(user_id)
-      .then((user) => {
-        let cart = req.session.cart;
-        let session = req.session;
+    findUserById(user_id).then((user) => {
+      let cart = req.session.cart;
+      let session = req.session;
 
-        if (!cart) {
-          return res.redirect('/');
-        }
+      if (!cart) {
+        return res.redirect("/");
+      }
 
-        const templateVars = { session, user, cart};
-        res.render("checkout", templateVars);
+      let totalCart = 0;
+      for (let tots of Object.values(cart.items)) {
+        totalCart += tots.item.price * tots.qty;
+      }
 
-      });
+      console.log("CART is", cart);
+      console.log("CART item  is", cart.items); //delete cart.items['1436']
+      console.log("Total price is", cart.totalPrice);
+      for (let list of Object.values(cart.items)) {
+        console.log("foods_id is:", list.item.id); //food_id
+        console.log("food name is:", list.item.title);
+        console.log("qty of food is:", list.qty);
+      }
+      const templateVars = { session, user, cart, totalCart };
+      res.render("checkout", templateVars);
+    });
   });
 
   // DELETE AN ITEM IN THE CART
-  router.post('/delete/:id', (req, res) => {
+  router.post("/delete/:id", (req, res) => {
     const user_id = req.session.user_id || "";
 
-    findUserById(user_id)
-      .then((user) => {
-        let cart = req.session.cart;
-        let session = req.session;
-        const id = req.params.id;
-        delete cart.items[id];
-        req.session.cart = cart;
-        const templateVars = { session, user, cart};
-        res.render("checkout", templateVars);
-      });
+    findUserById(user_id).then((user) => {
+      let cart = req.session.cart;
+      let session = req.session;
+      const id = req.params.id;
+      delete cart.items[id];
+      let totalCart = 0;
+      for (let tots of Object.values(cart.items)) {
+        totalCart += tots.item.price * tots.qty;
+      }
+      cart.totalPrice = totalCart;
+      req.session.cart = cart;
+      const templateVars = { session, user, cart, totalCart };
+      res.render("checkout", templateVars);
+    });
   });
 
   // EDIT THE QUANTITY
   router.post("/edit/:id", (req, res) => {
+    console.log("hellooooo");
     const user_id = req.session.user_id || "";
-    findUserById(user_id)
-      .then((user) => {
-        let cart = req.session.cart;
-        let session = req.session;
-        const id = req.params.id;
-        let newQty = req.body.editqty;
-        cart.items[id].qty = newQty;
-        req.session.cart = cart;
-        const templateVars = { session, user, cart};
-        res.render("checkout", templateVars);
-      });
 
+    findUserById(user_id).then((user) => {
+      let cart = req.session.cart;
+      let session = req.session;
+      const id = req.params.id;
+      let newQty = req.body.editqty;
+      cart.items[id].qty = newQty;
+
+
+
+      let totalCart = 0;
+      for (let tots of Object.values(cart.items)) {
+        totalCart += tots.item.price * tots.qty;
+      }
+      cart.totalPrice = totalCart;
+      req.session.cart = cart;
+      const templateVars = { session, user, cart, totalCart };
+      res.render("checkout", templateVars);
+    });
   });
 
   return router;
-
 };
 module.exports = checkoutRouter;
